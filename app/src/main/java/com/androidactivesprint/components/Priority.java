d7a9e790ac369076c7a69853e3030d3228f742b6
hungnv <hungnv@nexlesoft.com>
2017-10-02 22:05:44 +0700

    update

---
+++ app/src/main/java/com/androidactivesprint/components/Priority.java
@@ -0,0 +1,12 @@
+package com.androidactivesprint.components;
+
+/**
+ * Created by Hung on 10/2/2017.
+ */
+
+public enum Priority {
+
+    High,
+    Medium,
+    Low
+}
---
+++ app/src/main/java/com/androidactivesprint/components/Status.java
@@ -0,0 +1,12 @@
+package com.androidactivesprint.components;
+
+/**
+ * Created by Hung on 10/2/2017.
+ */
+
+public enum Status {
+
+    TO_DO,
+    IN_PROGRESS,
+    DONE
+}
